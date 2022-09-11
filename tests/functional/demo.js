c658b8855d41b1d0bdf5cdedfafa0100113fa41b
Chad Francis <yamanote1138@gmail.com>
2022-09-10 21:55:00 -0600

    trying to get functional tests flying

--- tests/functional/demo.js
+++ tests/functional/demo.js
@@ -1,27 +1,42 @@
-var prompt = require('prompt'),
-	wol = require('wake_on_lan');
+const prompt = require('prompt-async');
+const	wol = require('wake_on_lan');
+const { CersTVClient } = require('../../dist/index');
 
-prompt.start();
+const functionalTest = async () => {
+	prompt.start();
 
-var prompt_options = [
-	{
-		'name':'mac',
-		'default':'00:A0:96:84:FC:A0'
-	}
-];
+	var prompt_options = [
+		{
+			'name':'host',
+			'default':'192.168.1.23'
+		},
+		{
+			'name':'mac',
+			'default':'00:A0:96:84:FC:A0'
+		}
+	];
 
-prompt.get(prompt_options, function (err, result) {
-	wol.wake('20:DE:20:DE:20:DE', function(error) {
+	const { host, macAddress } = await prompt.get(prompt_options);
+
+	const tv = new CersTVClient(host, macAddress, 'test');
+
+	wol.wake(macAddress, (error) => {
 		if (error) {
 			console.log(error);
 		} else {
 			console.log('complete');
 		}
 	});
-});
 
-tv.sendIrCommand(cmd, function(err, res, body){
-	if(err) return console.log(err);
-	if(body) return console.log(body);
-	console.log(res.statusCode);
-});
+	tv.getRemoteCommandList().then((data) =>{
+		console.log(data);
+	});
+
+	// tv.sendIrCommand(cmd, function(err, res, body){
+	// 	if(err) return console.log(err);
+	// 	if(body) return console.log(body);
+	// 	console.log(res.statusCode);
+	// });
+};
+
+functionalTest();
